1c75999bba7f7c7c509fa0b1945093532ce049b0
Alvaro Abarca <aabarca@linets.cl>
2023-03-08 09:55:56 -0300

    Eslint fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 // import './App.css'
 
-
-import Landing from './pages/Landing';
+import Landing from './pages/Landing'
 // import Layout from './layouts/Layout';
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Navbar, Container, Nav, NavDropdown  } from "react-bootstrap";
+import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
 const Layout = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg='light' expand='lg'>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse className='justify-content-end'>
           <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
+            Signed in as: <a href='#login'>Mark Otto</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
