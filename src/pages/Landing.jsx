c6ae6de84ff64d451b84f315d2cf28d90372585c
Alvaro Abarca <dead.time.aa@gmail.com>
2023-03-09 22:39:29 -0300

    Elimino Comentarios

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -13,41 +13,6 @@ const Landing = () => {
         <Navbar bg='light' expand='false'>
           <Navbar.Brand to='/'>Ucb Melipilla</Navbar.Brand>
           <Navbar.Toggle aria-controls='navbarScroll' />
-          {/* <Navbar.Offcanvas>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand`}>
-              Offcanvas
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown
-              title="Dropdown"
-              id={`offcanvasNavbarDropdown-expand`}
-            >
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Offcanvas.Body>
-        </Navbar.Offcanvas> */}
           <Navbar.Collapse id='navbarScroll'>
             <Nav>
               <Nav.Link to='/'>Inicio</Nav.Link>
