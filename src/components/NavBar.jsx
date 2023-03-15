277fb21c220cc2a007bc0277b76452d98f73f8e2
Cris <cr.rojasmunoz@gmail.com>
2023-03-14 21:48:49 -0300

    NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
+            <a className="navbar-brand" href="/">Mi App</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav mr-auto justify-content-center">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/acerca-de">Acerca de</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/contacto">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
