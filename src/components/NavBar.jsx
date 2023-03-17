d0ed49b2188107d2209bf1a6b7802e62b943604e
Cris <cr.rojasmunoz@gmail.com>
2023-03-17 18:40:38 -0300

    se creó el efecto que cambia de color al deslizar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavBar = () => {
+    const [navStyle, setNavStyle] = useState({})
+
+    const handleScroll = () => {
+        const threshold = 50;
+        if (window.pageYOffset > threshold) {
+            setNavStyle({
+                backgroundColor: '#333',
+                position: 'sticky',
+                top: '0',
+                transition: 'background-color 0.5s, color 0.5s'
+            });
+        } else {
+            setNavStyle({
+                backgroundColor: 'transparent',
+                color: 'white',
+                position: 'sticky',
+                top: '0',
+                transition: 'background-color 0.5s, color 0.5s'
+            })
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-dark container-fluid" style={navStyle}>
             <a className="navbar-brand" href="/">Mi App</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -14,7 +44,19 @@ const NavBar = () => {
                         <a className="nav-link" href="/">Inicio</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/acerca-de">Acerca de</a>
+                        <a className="nav-link" href="/">Quiénes Somos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Ministerios</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">En qué creemos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Servicios</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/acerca-de">Prédicas</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/contacto">Contacto</a>
