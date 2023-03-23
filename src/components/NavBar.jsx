278597fd53ee44f2a21b02df35544d4b2714701b
Cris <cr.rojasmunoz@gmail.com>
2023-03-22 22:02:56 -0300

    Se añade Estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,65 +1,71 @@
 import React, { useState, useEffect } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
 
 const NavBar = () => {
-    const [navStyle, setNavStyle] = useState({})
+    const [backgroundStyle, setBackgroundStyle] = useState({
+        backgroundColor: 'transparent',
+    });
+
+    const textStyle = {
+        color: 'white',
+        position: 'sticky',
+        top: '0',
+        transition: 'background-color 0.5s, color 0.5s'
+    };
+
+    const linkStyle = {
+        color: 'black',
+        textDecoration: 'none'
+    };
 
     const handleScroll = () => {
         const threshold = 50;
         if (window.pageYOffset > threshold) {
-            setNavStyle({
-                backgroundColor: '#333',
-                position: 'sticky',
-                top: '0',
-                transition: 'background-color 0.5s, color 0.5s'
+            setBackgroundStyle({
+                backgroundColor: '#343a40', // Cambia a cualquier color que desees
             });
         } else {
-            setNavStyle({
+            setBackgroundStyle({
                 backgroundColor: 'transparent',
-                color: 'white',
-                position: 'sticky',
-                top: '0',
-                transition: 'background-color 0.5s, color 0.5s'
-            })
+            });
         }
-    }
+    };
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
+        window.addEventListener('scroll', handleScroll);
         return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
-
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark container-fluid" style={navStyle}>
-            <a className="navbar-brand" href="/">Mi App</a>
+        <nav className="navbar navbar-expand-lg container-fluid" style={{ ...backgroundStyle, ...textStyle }}>
+            <a className="navbar-brand" href="/" style={linkStyle}>Mi App</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav mr-auto justify-content-center">
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Inicio</a>
+                        <a className="nav-link" href="/" style={linkStyle}>Inicio</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Quiénes Somos</a>
+                        <a className="nav-link" href="/" style={linkStyle}>Quiénes Somos</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Ministerios</a>
+                        <a className="nav-link" href="/" style={linkStyle}>Ministerios</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">En qué creemos</a>
+                        <a className="nav-link" href="/" style={linkStyle}>En qué creemos</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Servicios</a>
+                        <a className="nav-link" href="/" style={linkStyle}>Servicios</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/acerca-de">Prédicas</a>
+                        <a className="nav-link" href="/acerca-de" style={linkStyle}>Prédicas</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/contacto">Contacto</a>
+                        <a className="nav-link" href="/contacto" style={linkStyle}>Contacto</a>
                     </li>
                 </ul>
             </div>
